e29c166d9981e8daecd7b009ffb1fad8990456b7
camille villard <hello@camillevillard.com>
2019-12-09 13:46:27 -0500

    categories controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
-class ApplicationController < ActionController::Base
+class Api::V1::ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+
+  def index
+    @categories = Category.all
+    render json: @categories
+  end
+
+  def show
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 end
