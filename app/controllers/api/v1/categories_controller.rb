6eb0c07ec5c16e8b03cdfdee41af62c1b1df8bf5
camille villard <hello@camillevillard.com>
2020-01-21 17:35:54 -0500

    controllers & routes

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,2 +1,20 @@
-class CategoriesController < ApplicationController
+class Api::V1::CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :update, :destroy]
+  skip_before_action :verify_authenticity_token
+
+  def index
+    @categories = Category.all
+    render json: @categories
+  end
+
+  def show
+    render json: @category, include: :products
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,2 @@
-class Api::V1::ApplicationController < ActionController::Base
-  skip_before_action :verify_authenticity_token
-
-  def index
-    @categories = Category.all
-    render json: @categories
-  end
-
-  def show
-  end
-
-  private
-
-  def set_category
-    @category = Category.find(params[:id])
-  end
+class ApplicationController < ActionController::Base
 end
